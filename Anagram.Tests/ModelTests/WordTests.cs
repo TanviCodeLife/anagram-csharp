604fbc52490648d2b2e76541842c9f10e9aac4db
Charley McGowan, Sheila Stephen and Tanvi Garg <sheila@>
2018-11-15 14:36:20 -0800

    create test methods for instantiation

--- Anagram.Tests/ModelTests/WordTests.cs
+++ Anagram.Tests/ModelTests/WordTests.cs
@@ -7,12 +7,67 @@ namespace Anagram.Models
   [TestClass]
   public class ItemTest
   {
+    // public void Dispose()
+    // {
+    //   Word.ClearAll();
+    // }
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Word testWord = new Word();
+      Word testWord = new Word("test");
       Assert.AreEqual(typeof(Word), testWord.GetType());
+      Console.WriteLine(testWord.GetType());
     }
+
+    [TestMethod]
+    public void Properties_ChecksFirstWordPropertiesInputCorrectly_String()
+    {
+      //Arrange
+      string testWord = "TestListString";
+      Word testWordObj = new Word(testWord);
+
+      //Act
+      string result = testWordObj.GetFirstWord();
+
+      //Assert
+      Assert.AreEqual(testWord, result);
+    }
+
+    [TestMethod]
+    public void Properties_ChecksInputListPropertyInput_String()
+    {
+      //Arrange
+      List<string> testList = new List<string>{};
+      string testInputWord = "TestListString";
+      testList.Add(testInputWord);
+      Word testWord = new Word(testList);
+
+      //Act
+      List<string> listResult = testWord.GetFirstList();
+
+      //Assert
+      CollectionAssert.AreEqual(testList, listResult);
+    }
+
+    [TestMethod]
+    public void Properties_ChecksPropertyInput_String()
+    {
+      //Arrange
+      List<string> testList = new List<string>{};
+      string testInputWord = "TestListString";
+      testList.Add(testInputWord);
+      Word testWord = new Word(testInputWord, testList);
+
+      //Act
+      string result = testWord.GetFirstWord();
+      List<string> listResult = testWord.GetFirstList();
+
+      //Assert
+      Assert.AreEqual(testInputWord, result);
+      CollectionAssert.AreEqual(testList, listResult);
+    }
+
+
   }
 }
--- Anagram/Models/Word.cs
+++ Anagram/Models/Word.cs
@@ -1,8 +1,34 @@
+using System.Collections.Generic;
+
 namespace Anagram.Models
 {
   public class Word
   {
+    private string _firstWord;
+    private List<string> _compareList;
 
+    public Word (string firstWord)
+    {
+      _firstWord = firstWord;
+    }
 
+    public Word(List<string> compareList) {
+
+      _compareList = compareList;
+    }
+
+    public Word (string firstWord, List<string> compareList)
+    {
+      _firstWord = firstWord;
+      _compareList = compareList;
+    }
+
+    public string GetFirstWord (){
+      return _firstWord;
+    }
+
+    public List<string> GetFirstList (){
+      return _compareList;
+    }
   }
 }
